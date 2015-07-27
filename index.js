35d9676fd43f353eb62f36e903168b059a514483
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-27 21:16:48 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/ping', function (req, res) {
+    res.send('pong');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Eslint-github listening at http://%s:%s', host, port);
+});
