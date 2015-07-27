4f69feb7fd4e22ef28131df5d66b8e91c8bd1388
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-27 21:28:12 +0200

    Add Procfile

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 8080));
+
 app.get('/ping', function (req, res) {
     res.send('pong');
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(app.get('port'), function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Eslint-github listening at http://%s:%s', host, port);
